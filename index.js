daa262690ddbcc43c2d0ed92fe323ca933dd0762
Rupanshu <mail2rupanshu@gmail.com>
2021-05-11 20:47:30 +0530

    setup initial directory structure

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const port = 8000;
+
+
+
+
+
+app.use("/",require("./routes"));
+
+app.listen(port,function(err){
+    if(err) console.log(`error in starting server: ${err}`);
+    console.log(`server is running on port: ${port}`)
+})
