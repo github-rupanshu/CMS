12e5913a132bc3915c135992fd34c496189fa9dc
Rupanshu <mail2rupanshu@gmail.com>
2021-06-01 18:49:49 +0530

    changed start script

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,23 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
 
-console.log('router loaded');
-
+console.log("router loaded");
 
 // router.get('/', homeController.home);
 // router.use('/users', require('./users'));
 // router.use('/posts', require('./posts'));
 // router.use('/comments', require('./comments'));
 
+router.use("/api", require("./api"));
 
-router.use('/api', require('./api'));
-
+router.get("/ping", (req, res) => {
+  return res.status(200).json({
+    msg: "Ping !!!",
+    gitUrl: "https://github.com/github-rupanshu/CMS.git",
+  });
+});
 // for any further routes, access from here
 // router.use('/routerName', require('./routerfile));
-
 
 module.exports = router;
