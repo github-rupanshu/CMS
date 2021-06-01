fd34e59cabc9b3db66487a544e1664c4ec45a015
Rupanshu <mail2rupanshu@gmail.com>
2021-06-01 19:05:25 +0530

    added ping url

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 8000 || process.env.PORT;
+const port = process.env.PORT||8000 ;
 const db = require("./config/mongoose");
 const jwt = require("./config/passportJWT");
 const passport = require("passport");
